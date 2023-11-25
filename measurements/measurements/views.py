ab112732e85767e0f45bde1fde791b9b9d3e79c3
Pablo <p.pedreros@uniandes.edu.co>
2023-11-25 12:06:38 -0500

    final

--- measurements/measurements/views.py
+++ measurements/measurements/views.py
@@ -42,7 +42,8 @@ def MeasurementCreate(request):
                 measurement.place = data_json['place']
                 measurement.save()
                 return HttpResponse("successfully created measurement")
-            else: HttpResponse("unsuccessfully created measurement. Place does not exist")
+            else: 
+                return HttpResponse("unsuccessfully created measurement. Place does not exist")
         else:
             return HttpResponse("unsuccessfully created measurement. Variable does not exist")
 
